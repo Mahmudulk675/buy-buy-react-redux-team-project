7e9e718b3011931fe2ccfae203ba571ceed5bc69
Mahmudulk675 <mahmudulk675@gmail.com>
2021-05-30 03:33:21 +0600

    dep

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 
-import './App.css';
+import './styles/style.scss';
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora fugiat debitis enim! Dolor fugiat ex natus, quisquam, incidunt mollitia reprehenderit qui aspernatur nulla sit quasi cum earum facere autem impedit quo corrupti consequuntur maxime blanditiis. Quam repellendus, aut aliquid debitis eveniet nemo tenetur quod magnam recusandae incidunt. Cum, quia quidem.
-      </p>
       
-     
+      
     </div>
   );
 }
